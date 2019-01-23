359ae9b6b009f2ba088b1bb4e519ee4bbb4ca563
dahby <dstoll243@gmail.com>
2019-01-23 13:10:50 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -61,4 +61,18 @@ class Person
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
+
+  def start_conversation(friend, topic)
+    if topic == "politics"
+      self.happiness -= 2
+      friend.happiness -= 2
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness += 1
+      friend.happiness += 1
+      "blah blah sun blah rain"
+    else
+      "blah blah blah blah blah"
+    end
+  end
 end
